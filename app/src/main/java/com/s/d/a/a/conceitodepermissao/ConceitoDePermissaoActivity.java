e9fca9b5d3fe51e74d1289ce3eb62877c9d4bfc5
Alexandre <aas@unochapeco.edu.br>
2019-02-20 16:38:51 -0300

    Mudanças para aprovação de permissões

--- app/src/main/java/com/s/d/a/a/conceitodepermissao/ConceitoDePermissaoActivity.java
+++ app/src/main/java/com/s/d/a/a/conceitodepermissao/ConceitoDePermissaoActivity.java
@@ -6,10 +6,14 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.support.v4.app.ActivityCompat;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class ConceitoDePermissaoActivity extends AppCompatActivity {
 
     private static String TAG = "ConceitoDePermissaoNoAndroid6ouSuperior";
+    private static final int CODIGO_DO_PEDIDO_DE_GRAVACAO = 199;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,52 @@ public class ConceitoDePermissaoActivity extends AppCompatActivity {
 
         if (permissao != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "Permissão para gravar negada!");
+            //fazerPedido();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Permissão para acessar o microfone solicitada para que esse app possa gravar áudio.")
+                        .setTitle("Permissão solicitada.");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.i(TAG, "Clicado.");
+                        fazerPedido();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                fazerPedido();
+            }
+        }
+    }
+
+    protected void fazerPedido(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO}, CODIGO_DO_PEDIDO_DE_GRAVACAO);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int codigoDoPedido,
+                                           String listaDePermissoes[], int[] resultadosConcedidos) {
+        switch (codigoDoPedido) {
+            case CODIGO_DO_PEDIDO_DE_GRAVACAO: {
+
+                if (resultadosConcedidos.length == 0
+                        || resultadosConcedidos[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permissão negada pelo usuário!");
+                } else {
+                    Log.i(TAG, "Permissão concedida pelo usuário!");
+                }
+                return;
+            }
         }
     }
 }
